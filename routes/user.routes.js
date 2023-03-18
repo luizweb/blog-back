d959f55cdea0521ed0e377fa016ec522c89ce230
Luiz Simoes <luizgsantonio@gmail.com>
2023-03-18 09:50:18 -0300

    update password

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -88,7 +88,7 @@ userRoute.post("/signup", async (req,res)=>{
             `
         }
   
-      await transporter.sendMail(mailOptions)
+        await transporter.sendMail(mailOptions)
 
 
         return res.status(201).json(createdUser);
@@ -112,7 +112,7 @@ userRoute.get('/activate-account/:id', async (req, res) => {
       console.log(error)
       res.status(500).json({ msg: 'Algo deu errado na ativação do usuário' })
     }
-  })
+});
 
 
 
@@ -124,7 +124,7 @@ userRoute.post("/login", async (req,res)=>{
         // Achar o user pelo email
         const user = await userModel.findOne({email: email});
 
-        // Chegar se o email existe
+        // Checar se o email existe
         if (user.confirmedEmail === false) {
             return res.status(401).json({ msg: 'Usuário não confirmado, favor validar email' })
         }
@@ -163,6 +163,81 @@ userRoute.post("/login", async (req,res)=>{
         return res.status(500).json(error);
     }
 });
+
+
+userRoute.post("/recoverpass", async (req, res) => {
+    try {
+               
+        const { email } = req.body;
+        
+        // Achar o user pelo email
+        const user = await userModel.findOne({email: email});
+
+        // Checar se o email foi validado
+        if (user.confirmedEmail === false) {
+            return res.status(401).json({ msg: 'Usuário não confirmado, favor validar email' })
+        }
+
+        // Chegar se o usuário existe
+		if (!user){
+            return res.status(404).json({msg: "email inválido"});
+        }
+
+        console.log("Enviar email:")
+        console.log(email)
+        /* // Envia email com orientação
+        const mailOptions = {
+            from: 'luiz.agsimoes@outlook.com',
+            to: email,
+            subject: 'Alteração de senha do site Luiz Simoes',
+            html: `
+            <h1>Solicitação de alteração de senha</h1>
+            <p>Clicando no link a seguir você será direcinoando a uma página para realizar a alteração da senha: </p>
+            <a href="https://luizweb.netlify.app/updatepass/${user._id}">Alterar senha de acesso</a>
+            `
+        } 
+
+        await transporter.sendMail(mailOptions)*/
+
+        return res.status(200).json({msg: "email de recuperacao de senha enviado"})
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+});
+
+
+userRoute.put("/updatepass/:id", async (req,res)=>{
+    try {
+        const {id} = req.params;
+        const {password} = req.body;        
+        
+        // Checando se a senha existe e se atende aos critérios de segurança
+        if (!password || !password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/)) {
+            return res.status(400).json({ message: "Senha não tem os requisitos necessários." });
+        }
+
+        // Gerar o salt da senha
+        const salt = await bcrypt.genSalt(SALT_ROUNDS);
+        
+        // Hashear a senha
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Update do usuário com a senha hasheada
+        const updateUser = await userModel.findByIdAndUpdate(id, {passwordHash: hashedPassword}, {new: true, runValidators: true});
+        
+        // Deleta o passwordHash do obj antes de retornar a resposta
+        delete updateUser._doc.passwordHash;       
+        
+        return res.status(200).json(updateUser);
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error.errors);
+    }
+});
+
 
 userRoute.get("/profile", isAuth, attachCurrentUser, async (req, res) => {
     try {
@@ -216,6 +291,7 @@ userRoute.delete("/delete/:id", async (req,res)=>{
 userRoute.put("/update/:id", async (req,res)=>{
     try {
         const {id} = req.params;
+        
         const updateUser = await userModel.findByIdAndUpdate(id, {...req.body}, {new: true, runValidators: true});
         return res.status(200).json(updateUser);
     } catch (error) {
